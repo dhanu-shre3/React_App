620e48e4091bf45ca9a7458cc13501e7b9b4a765
Dhanushree Motanalira Somaiah <motanalirasomaiahd@cardiff.ac.uk>
2025-03-19 10:36:36 +0000

    react

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,11 @@
+function Message(){
+    //javascript XML
+    const name = 'Dhanu';
+    if (name){
+        return <h1>Hello {name}</h1>
+        return <h1>Hello World</h1>;
+    }
+
+}
+
+export default Message;
