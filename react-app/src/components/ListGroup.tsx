5a8ac56acc0e363998b48da4e1a3b890edc312c7
Dhanushree Motanalira Somaiah <motanalirasomaiahd@cardiff.ac.uk>
2025-03-19 13:43:13 +0000

    event handling

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,14 +1,36 @@
+import { MouseEvent } from "react";
+import { Fragment } from "react";
 function ListGoup() {
+    let items = [
+        'New York',
+        'San Francisco',
+        'Tokyo',
+        'London',
+        'Paris'
+    ];
+
+    //Event handling
+    const handleClick = (event: MouseEvent) => console.log(event);
+ 
+
   return (
-    <ul className="list-group">
-      <li className="list-group-item active" aria-current="true">
-        An active item
-      </li>
-      <li className="list-group-item">A second item</li>
-      <li className="list-group-item">A third item</li>
-      <li className="list-group-item">A fourth item</li>
-      <li className="list-group-item">And a fifth one</li>
-    </ul>
+    <Fragment>
+        <h1>List</h1> 
+         {items.length === 0 && <p>No item found</p>}
+        <ul className="list-group">
+           {items.map((item, index) => (
+            <li 
+               className= "list-group-item" 
+               key={item} 
+               onClick={handleClick}
+               >
+                {item}
+            </li>
+            
+            ))}
+        
+        </ul>
+    </Fragment>
   );
 }
 
